899d1f695bb864611d98ebb365ab841f4833772d
Valeriia Ten <tenv279@gmail.com>
2020-09-03 20:53:35 -0400

    Render my own module

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Item-Details-ValeriiaTen-Etsy-Proxy
+
+Proxy server - Port 8000
+Details module server - Port 5000
+Reviews module server - Port 8888
+Pictures module server - Port 3000
--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
-
+const path = require('path');
+require('dotenv').config();
+const cors = require('cors')
+const axios = require('axios').default;
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/../client/dist'));
+const port = process.env.PORT || 8000;
 
-const port = process.env.PORT || 5555;
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/:productId', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.get('/itemDetails/:productId', (req, res) => {
+  axios.get(`http://localhost:5000${req.path}`)
+    .then(({ headers, data }) => {
+      // handle success
+      res.setHeader('content-type', headers['content-type']);
+      res.send(data);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
+})
 
 app.listen(port, () => {
   console.log(`Proxy Server listening on port ${port}`);
